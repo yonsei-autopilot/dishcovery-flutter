f07e43e84cb2751b4320253f3612913ec71352ed
haaem <hyemin17@yonsei.ac.kr>
2025-04-30 20:26:43 +0900

    💄 Design: font 추가
    config/theme 추가

---
+++ lib/src/config/theme/color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const primaryNavy = Color(0xff394C6D);
+const primaryWhite = Colors.white;
+const primaryBlack = Color(0xff000000);
+const primaryGrey = Color(0xff999999);
+const primaryLightGreen = Color(0xffC1DFC5);
+const primaryLightGreen2 = Color(0xff9FBD5D);
+const primaryGreenButton = Color(0xff85C28B);
+const primaryPink = Color(0xffFFE1E1);
+const primaryPinkButton = Color(0xffEABBE5);
+const primaryYellow = Color(0xffFFF5E1);
+const primaryYellowButton = Color(0xffC9CD9E);
+const primarySkyBlue = Color(0xffE1F6FF);
+const primarySkyBlueButton = Color(0xff85AFCE);
+
+Color backgroundColor = primaryWhite;
+Color buttonColor = primaryGreenButton;
