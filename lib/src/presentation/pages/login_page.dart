2f3729a95f58e71eb087c004713f7e4dd981138a
Myeonghoon Lee <11175606+mhl6829@users.noreply.github.com>
2025-05-01 20:43:04 +0900

    ✨ Feat: riverpod DI 추가

--- lib/src/presentation/pages/login_page.dart
+++ lib/src/presentation/pages/login_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:smart_menu_flutter/src/presentation/providers/providers.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final loginUseCase = ref.read(loginUseCaseProvider);
+
     return Scaffold(
-      body: Container(),
+      body: ElevatedButton(
+        onPressed: () {
+          loginUseCase.call();
+        },
+        child: const Text('로그인'),
+      ),
     );
   }
 }
