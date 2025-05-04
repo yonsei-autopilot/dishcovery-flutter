ad6a613ae668d30bf99c097658e5fe9fb3c17376
haaem <hyemin17@yonsei.ac.kr>
2025-05-05 00:55:15 +0900

    💄 Design: 로그인 페이지 UI 추가

--- lib/src/config/theme/color.dart
+++ lib/src/config/theme/color.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-const primaryRed = Color(0xffff8f8f);
+const primaryRed = Color(0xffE94235);
+const primaryGreen = Color(0xff678D58);
 const primaryWhite = Colors.white;
 const primaryBlack = Color(0xff333333);
-const primaryGrey = Color(0xff999999);
+const primaryGrey = Color(0xff7D7D7D);
 
 Color backgroundColor = primaryWhite;
-Color buttonColor = primaryRed;
+Color mainColor = primaryGreen;
