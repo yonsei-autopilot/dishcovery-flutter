400d515cfe3663de73738829fb0fcddc832247b0
정준우 <80523328+bviz6542@users.noreply.github.com>
2025-05-05 09:34:05 +0900

    ✨ api response 공통화

---
+++ lib/src/domain/dtos/common/api_response.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ApiResponse<T> _$ApiResponseFromJson<T>(
+  Map<String, dynamic> json,
+  T Function(Object? json) fromJsonT,
+) =>
+    ApiResponse<T>(
+      isSuccess: json['isSuccess'] as bool,
+      data: _$nullableGenericFromJson(json['data'], fromJsonT),
+      error: ApiResponse._errorFromJson(json['error']),
+    );
+
+Map<String, dynamic> _$ApiResponseToJson<T>(
+  ApiResponse<T> instance,
+  Object? Function(T value) toJsonT,
+) =>
+    <String, dynamic>{
+      'isSuccess': instance.isSuccess,
+      'data': _$nullableGenericToJson(instance.data, toJsonT),
+      'error': ApiResponse._errorToJson(instance.error),
+    };
+
+T? _$nullableGenericFromJson<T>(
+  Object? input,
+  T Function(Object? json) fromJson,
+) =>
+    input == null ? null : fromJson(input);
+
+Object? _$nullableGenericToJson<T>(
+  T? input,
+  Object? Function(T value) toJson,
+) =>
+    input == null ? null : toJson(input);
