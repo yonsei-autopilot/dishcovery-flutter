74f4cf5c80041e69186f4a71757e1ef9cf625097
haaem <serrysallylee@gmail.com>
2025-05-06 15:35:27 +0900

    🐛 Fix: menu_detail_response.dart 수정

--- lib/src/domain/dtos/menu/menu_detail_response.dart
+++ lib/src/domain/dtos/menu/menu_detail_response.dart
@@ -1,34 +1,34 @@
 import 'package:json_annotation/json_annotation.dart';
-//
-// part 'menu_detail_response.g.dart';
-//
+
+part 'menu_detail_response.g.dart';
+
 @JsonSerializable()
 class MenuDetailResponse {
-//   @JsonKey(name: 'items')
-//   final List<MenuResponse> item;
-//
-//   MenuDetailResponse({required this.item});
-//
-//   factory MenuDetailResponse.fromJson(Map<String, dynamic> json) =>
-//       _$MenuDetailResponseFromJson(json);
-//   Map<String, dynamic> toJson() => _$MenuDetailResponseToJson(this);
+  @JsonKey(name: 'items')
+  final List<MenuResponse> item;
+
+  MenuDetailResponse({required this.item});
+
+  factory MenuDetailResponse.fromJson(Map<String, dynamic> json) =>
+      _$MenuDetailResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$MenuDetailResponseToJson(this);
 }
-//
-// @JsonSerializable()
-// class MenuResponse {
-//   final String imageUrl;
-//   final String menuName;
-//   final String menuDescription;
-//   final String foodAversion;
-//
-//   MenuResponse({
-//     required this.imageUrl,
-//     required this.menuName,
-//     required this.menuDescription,
-//     required this.foodAversion,
-//   });
-//
-//   factory MenuResponse.fromJson(Map<String, dynamic> json) =>
-//       _$MenuResponseFromJson(json);
-//   Map<String, dynamic> toJson() => _$MenuResponseToJson(this);
-// }
+
+@JsonSerializable()
+class MenuResponse {
+  final String imageUrl;
+  final String menuName;
+  final String menuDescription;
+  final String foodAversion;
+
+  MenuResponse({
+    required this.imageUrl,
+    required this.menuName,
+    required this.menuDescription,
+    required this.foodAversion,
+  });
+
+  factory MenuResponse.fromJson(Map<String, dynamic> json) =>
+      _$MenuResponseFromJson(json);
+  Map<String, dynamic> toJson() => _$MenuResponseToJson(this);
+}
