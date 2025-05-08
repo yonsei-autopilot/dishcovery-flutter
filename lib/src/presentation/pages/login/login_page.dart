3066f4cbef0e30d78019c2ea0c48ab80d08ad29e
haaem <hyemin17@yonsei.ac.kr>
2025-05-09 01:52:36 +0900

    🐛 Fix: 로그인 페이지 overflow 해결 & Design: UI 업데이트

--- lib/src/presentation/pages/login/login_page.dart
+++ lib/src/presentation/pages/login/login_page.dart
@@ -37,126 +37,157 @@ class LoginPageState extends ConsumerState<LoginPage> {
         backgroundColor: primaryWhite,
         body: SafeArea(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 20,),
-                Image.asset(
-                  'assets/images/green_logo.png',
-                  width: 88,
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                // id
-                Container(
-                  height: 58,
-                  width: width - 110,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(29),
-                    color: const Color(0xffD9D9D9),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 20,),
+                  Image.asset(
+                    'assets/images/green_logo.png',
+                    width: 88,
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 29),
-                  alignment: Alignment.centerLeft,
-                  child: TextFormField(
-                    controller: _loginIdController,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Login Id',
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  // id
+                  Container(
+                    height: 58,
+                    width: width - 110,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(29),
+                      color: const Color(0xffD9D9D9),
                     ),
-                    style: const TextStyle(fontSize: 14, color: primaryGrey),
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                // password
-                Container(
-                  height: 58,
-                  width: width - 110,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(29),
-                    color: const Color(0xffD9D9D9),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 29),
-                  alignment: Alignment.centerLeft,
-                  child: Center(
+                    padding: const EdgeInsets.symmetric(horizontal: 29),
+                    alignment: Alignment.centerLeft,
                     child: TextFormField(
-                      controller: _passwordController,
-                      obscureText: true,
+                      controller: _loginIdController,
                       decoration: const InputDecoration(
                         border: InputBorder.none,
-                        hintText: 'Password',
-                        suffixIcon: Icon(CupertinoIcons.eye_slash, color: primaryGrey),
-                        contentPadding: EdgeInsets.symmetric(vertical: 18),
+                        hintText: 'Login Id',
                       ),
                       style: const TextStyle(fontSize: 14, color: primaryGrey),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                const Row(
-                  children: [
-                    SizedBox(
-                      width: 70,
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  // password
+                  Container(
+                    height: 58,
+                    width: width - 110,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(29),
+                      color: const Color(0xffD9D9D9),
                     ),
-                    Icon(CupertinoIcons.circle, size: 10, color: primaryGrey),
-                    SizedBox(
-                      width: 2,
+                    padding: const EdgeInsets.symmetric(horizontal: 29),
+                    alignment: Alignment.centerLeft,
+                    child: Center(
+                      child: TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Password',
+                          suffixIcon: Icon(CupertinoIcons.eye_slash, color: primaryGrey),
+                          contentPadding: EdgeInsets.symmetric(vertical: 18),
+                        ),
+                        style: const TextStyle(fontSize: 14, color: primaryGrey),
+                      ),
                     ),
-                    BodyText(
-                      text: "Remember Me",
-                      size: 12,
-                      color: primaryGrey,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 150,
-                ),
-                // google login button
-                GestureDetector(
-                  child: Image.asset(
-                    'assets/images/google_login.png',
-                    height: 34,
                   ),
-                  onTap: () {
-                    ref.read(authNotifierProvider.notifier).googleLogin();
-                  },
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                // login button
-                GestureDetector(
-                  child: Container(
-                      decoration: BoxDecoration(color: mainColor, borderRadius: BorderRadius.circular(24)),
-                      height: 50,
-                      width: width - 110,
-                      child: const Center(child: BodyText(text: 'Login', color: primaryWhite, size: 20,))
+                  const SizedBox(
+                    height: 25,
                   ),
-                  onTap: () {
-                    final loginId = _loginIdController.text.trim();
-                    final password = _passwordController.text.trim();
-                    ref.read(authNotifierProvider.notifier).simpleLogin(loginId, password);
-                  }
-                ),
-                // 임시 버튼
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  child: const BodyText(
-                    text: 'camera page',
-                    color: primaryRed,
+                  const Row(
+                    children: [
+                      SizedBox(
+                        width: 70,
+                      ),
+                      Icon(CupertinoIcons.circle, size: 10, color: primaryGrey),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      BodyText(
+                        text: "Remember Me",
+                        size: 12,
+                        color: primaryGrey,
+                      )
+                    ],
                   ),
-                  onPressed: () {
-                    ref.read(authNotifierProvider.notifier).test();
-                  },
-                )
-              ],
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  // login button
+                  GestureDetector(
+                      child: Container(
+                          decoration: BoxDecoration(
+                              color: mainColor,
+                              borderRadius: BorderRadius.circular(24),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black.withOpacity(0.4),
+                                    spreadRadius: 2,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 3)
+                                )
+                              ]
+                          ),
+                          height: 50,
+                          width: width - 110,
+                          child: const Center(child: BodyText(text: 'Log In', color: primaryWhite, size: 20,))
+                      ),
+                      onTap: () {
+                        final loginId = _loginIdController.text.trim();
+                        final password = _passwordController.text.trim();
+                        ref.read(authNotifierProvider.notifier).simpleLogin(loginId, password);
+                      }
+                  ),
+                  const SizedBox(height: 30,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: (width-155)/2,
+                        height: 1,
+                        color: const Color(0xffCFCFCF),
+                      ),
+                      const BodyText(text: "OR", color: const Color(0xffCFCFCF), size: 12,),
+                      Container(
+                        width: (width-155)/2,
+                        height: 1,
+                        color: const Color(0xffCFCFCF),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20,),
+                  // google login button
+                  GestureDetector(
+                    child: Image.asset(
+                      'assets/images/google_login.png',
+                      height: 34,
+                    ),
+                    onTap: () {
+                      ref.read(authNotifierProvider.notifier).googleLogin();
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  // 임시 버튼
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    child: const BodyText(
+                      text: 'camera page',
+                      color: primaryRed,
+                    ),
+                    onPressed: () {
+                      ref.read(authNotifierProvider.notifier).test();
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ));
