c040f5429cf00777097ca6e0a44187d215e56b27
정준우 <80523328+bviz6542@users.noreply.github.com>
2025-05-10 22:25:07 +0900

    ✨ 메뉴 상세정보 api 부착

---
+++ lib/src/domain/dtos/menu/menu_explanation_request.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'menu_explanation_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MenuExplanationRequest _$MenuExplanationRequestFromJson(
+        Map<String, dynamic> json) =>
+    MenuExplanationRequest(
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$MenuExplanationRequestToJson(
+        MenuExplanationRequest instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+    };
